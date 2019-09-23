e749e698dae1165659a69e561116d5502a02acfc
danilopereira10 <54330234+danilopereira10@users.noreply.github.com>
2019-09-23 18:38:14 -0300

    Upgrade

--- lab03/lab03.c
+++ lab03/lab03.c
@@ -3,20 +3,34 @@
 #include "livro.h"
 #include "leitor.h"
 
-void imprime_saida(int quantidadeDeLeitoresAtivos, Leitor* leitores) {
-	for (int contador = 0; contador < quantidadeDeLeitoresAtivos; contador = contador + 1) {
+int quantidadeLeitoresAtivos;
+Data dataConsulta;
 
-		//leitorParaEnviar criado para poder invocar a função obter_nome, que requere ponteiro como parâmetro
-		Leitor* leitorParaEnviar = &(leitores[contador]);
-		printf(obter_nome(leitorParaEnviar));
-		printf("\n");
-		int num_rest = obter_num_restituicoes(leitorParaEnviar);	
-		for (int contadorDeLivros = 0; contador < num_rest; contadorDeLivros = contadorDeLivros + 1) {
-			//nothing yet
+void copiar_string(char destino[], const char origem[]) {
+	int i = 0;
+	for (; origem[i] != '\0'; i = i + 1) {
+		destino[i] = origem[i];
+	}
+	destino[i] = '\0';
+}
+
+void imprimeSaida(Leitor* leitores, Livro* livros) {
+	int contadorLeitores = 0;;
+	//int contadorLivros; 
+	Leitor* leitorParaSerImpresso = &leitores[contadorLeitores];
+	printf(obter_nome(leitorParaSerImpresso));
+	printf("\n");
+	Leitor* leitorAtual = &(leitores[contadorLeitores]);
+	const int* restituicao = obter_restituicoes(leitorAtual);
+	int num_rest = obter_num_restituicoes(leitorAtual);
+	for (int contador = 0; contador < num_rest; contador = contador + 1) {
+		Livro* livroAtual = &livros[restituicao[contador]];
+		Data dataDeEmprestimo = obter_data_emprestimo(livroAtual);
+		if (eh_menor_que(dataDeEmprestimo, dataConsulta)) {
+			printf("%d", restituicao[contador]);	
 		}
 	}
-	//imprimeLeitorNaOrdemEmQueLeu();
-	//imprimeLivrosDoLeitor();
+	
 }
 
 void lerDadosDosLivros(int quantidadeLivrosRegistrados, char titulo[], Livro* livros) {
@@ -44,21 +58,21 @@ void registrarRestituicoesDevolucoes(Leitor* leitorAtual, int numeroDeEmprestimo
 	}
 }
 
-void lerDadosDosLeitores(int quantidadeLeitoresAtivos, char nomeDoLeitor[], Leitor* leitores, Leitor* leitorAtual) {
+void lerDadosDosLeitores(int quantidadeLeitoresAtivos, Leitor* leitores, char nome[]) {
 	for (int contador = 0; contador < quantidadeLeitoresAtivos; contador = contador + 1) {
-		scanf(" %[^\n]", nomeDoLeitor);
+		scanf(" %[^\n]", nome);
 		
 		int numeroDeEmprestimosDevolucoes;
 		scanf("%d", &numeroDeEmprestimosDevolucoes);
-		leitores[quantidadeLeitoresAtivos] = criar_leitor(nomeDoLeitor, numeroDeEmprestimosDevolucoes);
+		leitores[quantidadeLeitoresAtivos] = criar_leitor(nome, numeroDeEmprestimosDevolucoes);
+		Leitor* leitorAtual = &leitores[quantidadeLeitoresAtivos];
 		registrarRestituicoesDevolucoes(leitorAtual, numeroDeEmprestimosDevolucoes);
 	}
 }
 
-void lerEntrada(Livro* livros, Leitor* leitores, char titulo[]) {
+void lerEntrada(Livro* livros, Leitor* leitores, char titulo[], char nome[]) {
 	//declara variáveis
 	int quantidadeLivrosRegistrados;
-	int quantidadeLeitoresAtivos;
 
 	//recebe entrada
 	scanf("%d", &quantidadeLivrosRegistrados);
@@ -68,7 +82,7 @@ void lerEntrada(Livro* livros, Leitor* leitores, char titulo[]) {
 	leitores = malloc(quantidadeLeitoresAtivos * sizeof(Leitor*));
 
 	lerDadosDosLivros(quantidadeLivrosRegistrados, titulo, livros);
-	lerDadosDosLeitores(quantidadeLeitoresAtivos, leitores, leitorAtual);
+	lerDadosDosLeitores(quantidadeLeitoresAtivos, leitores, nome);
 
 	int dia;
 	int mes;
@@ -88,8 +102,8 @@ int main()
 	Livro* livros = NULL;        // ponteiro para o vetor de livros
 	Leitor* leitores = NULL;     // ponteiro para o vetor de leitores
 
-	lerEntrada(Livro* livros, Leitor* leitores, titulo);
-	imprime_saida(quantidadeLeitoresAtivos, leitores);
+	lerEntrada(livros, leitores, titulo, nome);
+	imprimeSaida(leitores, livros);
 
 	desalocar_memoria(leitores);
 	free(livros);
@@ -97,28 +111,3 @@ int main()
 	return 0;
 }
 
-
-void copiar_string(char destino[], const char origem[]) {
-	int i = 0;
-	for (; origem[i] != '\0'; i = i + 1) {
-		destino[i] = origem[i];
-	}
-	destino[i] = '\0';
-}
-
-
-void imprimeSaida(Leitor* leitores, Livro* livros, int contadorLeitores, int contadorLivros, Data dataConsulta) {
-	printf(obter_nome(leitores[contadorLeitores]));
-	printf("\n");
-	Leitor* leitorAtual = &(leitores[contadorLeitores]);
-	int* restituicao = obter_restituicoes(leitorAtual);
-	int num_rest = obter_num_restituicoes(leitorAtual);
-	for (int contador = 0; contador < num_rest; contador = contador + 1) {
-		Livro* livroAtual = livros[restituicao[contador]];
-		Data dataDeEmprestimo = obter_data_emprestimo(livroAtual);
-		if (eh_menor_que(dataDeEmprestimo < dataConsulta)) {
-			printf(restituicao[contador]);	
-		}
-	}
-	
-}
